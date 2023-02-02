1ee795a3f7537e364d7688ed81794d0ac4081220
Mauricio <mauricio.cds00@gmail.com>
2023-02-02 10:19:21 -0300

    feat: message producer

--- cmd/generator/main.go
+++ cmd/generator/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	index := 0
+	for {
+		index++
+		produce([]byte(fmt.Sprintf("Message: %d\n", index)), "nfe")
+		fmt.Printf("Message: %d", index)
+	}
+}
+
+func produce(message []byte, topic string) {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "host.docker.internal:9094",
+	}
+
+	kafkaProducer, err := kafka.NewProducer(configMap)
+
+	if err != nil {
+		panic(err)
+	}
+
+	kafkaProducer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          message,
+	}, nil)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mauFade/kafka-go
 
 go 1.19
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2
