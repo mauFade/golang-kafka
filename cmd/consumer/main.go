0283eed652a0e1e72d2abf84afb65c88f3b9f6fd
Mauricio <mauricio.cds00@gmail.com>
2023-02-05 15:24:28 -0300

    feat: init consumer function

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,5 +1,11 @@
 package main
 
+import "github.com/confluentinc/confluent-kafka-go/kafka"
+
 func main() {
+
+}
+
+func Consume(topics []string, server string, msgChannel chan *kafka.Message) {
 
 }
