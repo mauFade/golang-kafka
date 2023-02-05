b95273c7f5df0e75941dfb38d29c68d336ace39f
Mauricio <mauricio.cds00@gmail.com>
2023-02-05 15:37:17 -0300

    feat: main consumer function

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -3,7 +3,17 @@ package main
 import "github.com/confluentinc/confluent-kafka-go/kafka"
 
 func main() {
+	msgChan := make(chan *kafka.Message)
+	topics := []string{"nfe"}
+	servers := "host.docker.internal: 9094"
 
+	// Gera uma thread nova  e fica lendo dados do kafka
+	go Consume(topics, servers, msgChan)
+
+	for {
+		msg := <-msgChan
+		println(string(msg.Value))
+	}
 }
 
 func Consume(topics []string, servers string, msgChannel chan *kafka.Message) {
